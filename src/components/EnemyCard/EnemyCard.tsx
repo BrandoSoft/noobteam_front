1eea03a96637c5b394c59212179da3e6d7fa4797
BrandoSoft <csbrandox@gmail.com>
2022-07-13 19:40:33 +0200

    Added champion card with stats + some .png with tier icons

--- src/components/EnemyCard/EnemyCard.tsx
+++ src/components/EnemyCard/EnemyCard.tsx
@@ -3,94 +3,99 @@ import React, {useEffect, useState} from 'react';
 import './EnemyCard.scss'
 import {useSelector} from "react-redux";
 import {RootState} from "../../redux/store";
-// import {LeaguesEntity} from 'types'
+import {LeaguesEntity} from 'types'
+import EnemyStats from "./EnemyStats";
 
 
 const EnemyCard = ({data, list}: any) => {
 
-        const {userToken} = useSelector((store: RootState) => store.user);
+    const {userToken} = useSelector((store: RootState) => store.user);
 
-        const [leagueInfo, setLeagueInfo] = useState(null)
+    const [leagueInfo, setLeagueInfo] = useState<LeaguesEntity[] | []>([])
 
 
-        const version = process.env.REACT_APP_DDRAGON;
-        let champName = ''
+    const version = process.env.REACT_APP_DDRAGON;
+    let champName = ''
 
-        for (let i in list) {
-            if (list[i].key == data.championId) {
-                champName = list[i].id
-            }
+    for (let i in list) {
+        if (list[i].key == data.championId) {
+            champName = list[i].id
         }
+    }
 
-        //@TODO Pobieranie historii meczy danego gracza niemożliwe, z pod ograniczeń aktualnego klucza API. Oczekuje na akceptację wniosku udostępnienia klucza developerskiego.
+    //@TODO Pobieranie historii meczy danego gracza niemożliwe, z pod ograniczeń aktualnego klucza API. Oczekuje na akceptację wniosku udostępnienia klucza developerskiego.
 
-        useEffect(() => {
-            // if (data) {
-                (async () => {
-                    const res = await fetch(`${process.env.REACT_APP_BACKEND}/characters/find/${data.summonerName}`, {
-                            method: 'GET',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'x-auth-token': userToken,
-                            },
-                        }
-                    );
-                    const summonerData = await res.json()
-                    console.log('summoner data id: ', summonerData.id)
+    useEffect(() => {
+        // if (data) {
+        (async () => {
+            const res = await fetch(`${process.env.REACT_APP_BACKEND}/characters/find/${data.summonerName}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-auth-token': userToken,
+                    },
+                }
+            );
+            const summonerData = await res.json()
+            console.log('summoner data id: ', summonerData.id)
 
-                    const league = await fetch(`${process.env.REACT_APP_BACKEND}/characters/leagues/${summonerData.id}`, {
-                            method: 'GET',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'x-auth-token': userToken,
-                            },
-                        }
-                    );
-                    const summonerLeague = await league.json()
-                    setLeagueInfo(summonerLeague)
-
-                    console.log('league info: ',leagueInfo)
-
-                    // setSummonerInfo(summonerData)
-                    //
-                    //             const resGameHistory = await fetch(`${process.env.REACT_APP_BACKEND}/matches/playermatches/${summonerData.puuid}`, {
-                    //                     method: 'GET',
-                    //                     headers: {
-                    //                         'Content-Type': 'application/json',
-                    //                         'x-auth-token': userToken,
-                    //                     },
-                    //                 }
-                    //             );
-                    //             const gameHistory = await resGameHistory.json()
-                    //             // console.log(gameHistory)
-                    //
-                    //             for (const e of gameHistory) {
-                    //                 const resInfo = await fetch(`${process.env.REACT_APP_BACKEND}/matches/matchinfo/${e}`, {
-                    //                         method: 'GET',
-                    //                         headers: {
-                    //                             'Content-Type': 'application/json',
-                    //                             'x-auth-token': userToken,
-                    //                         },
-                    //                     }
-                    //                 );
-                    //                 setLast(await resInfo.json())
-                    //             }
-                })()
-
-            // }
-        }, [])
+            const league = await fetch(`${process.env.REACT_APP_BACKEND}/characters/leagues/${summonerData.id}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'x-auth-token': userToken,
+                    },
+                }
+            );
+            const summonerLeague = await league.json()
+            setLeagueInfo(summonerLeague)
 
 
-        return (
+            // setSummonerInfo(summonerData)
+            //
+            //             const resGameHistory = await fetch(`${process.env.REACT_APP_BACKEND}/matches/playermatches/${summonerData.puuid}`, {
+            //                     method: 'GET',
+            //                     headers: {
+            //                         'Content-Type': 'application/json',
+            //                         'x-auth-token': userToken,
+            //                     },
+            //                 }
+            //             );
+            //             const gameHistory = await resGameHistory.json()
+            //             // console.log(gameHistory)
+            //
+            //             for (const e of gameHistory) {
+            //                 const resInfo = await fetch(`${process.env.REACT_APP_BACKEND}/matches/matchinfo/${e}`, {
+            //                         method: 'GET',
+            //                         headers: {
+            //                             'Content-Type': 'application/json',
+            //                             'x-auth-token': userToken,
+            //                         },
+            //                     }
+            //                 );
+            //                 setLast(await resInfo.json())
+            //             }
+        })()
+
+        // }
+    }, [])
+
+    console.log('league table',leagueInfo)
+
+
+    return (
             <div className="enemy">
                 <img
                     src={`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champName}.png`}
                     alt=""/>
                 <p className="enemy__name">{data.summonerName}</p>
-
+                <>
+                    {leagueInfo.length > 0? leagueInfo.map( league=> <EnemyStats key={league.leagueId + league.summonerId} stats={league}/>) : null}
+                    </>
             </div>
-        );
-    }
-;
+
+    );
+}
+
 
 export default EnemyCard;
---
+++ src/components/EnemyCard/EnemyStats.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import {LeaguesEntity} from 'types'
+
+import Iron from '../../img/tiers/Emblem_Iron.png'
+import Silver from '../../img/tiers/Emblem_Silver.png'
+import Gold from '../../img/tiers/Emblem_Gold.png'
+import Platinum from '../../img/tiers/Emblem_Platinum.png'
+import Diamond from '../../img/tiers/Emblem_Diamond.png'
+import Master from '../../img/tiers/Emblem_Master.png'
+import Grandmaster from '../../img/tiers/Emblem_Grandmaster.png'
+import Bronze from '../../img/tiers/Emblem_Bronze.png'
+import Challenger from '../../img/tiers/Emblem_Challenger.png'
+
+const getImage = (tier: string) => {
+    switch (tier) {
+        case "GOLD":
+            return <img src={Gold} alt="tier badge, in this case its Gold Bage"/>
+        case "IRON":
+            return <img src={Iron} alt="tier badge, in this case its Iron Bage"/>
+        case "BRONZE":
+            return <img src={Bronze} alt="tier badge, in this case its Bronze Bage"/>
+        case "SILVER":
+            return <img src={Silver} alt="tier badge, in this case its Silver Bage"/>
+        case "PLATINUM":
+            return <img src={Platinum} alt="tier badge, in this case its Platinum Bage"/>
+        case "DIAMOND":
+            return <img src={Diamond} alt="tier badge, in this case its Diamond Bage"/>
+        case "MASTER":
+            return <img src={Master} alt="tier badge, in this case its Master Bage"/>
+        case "GRANDMASTER":
+            return <img src={Grandmaster} alt="tier badge, in this case its Grandmaster Bage"/>
+        case "CHALLENGER":
+            return <img src={Challenger} alt="tier badge, in this case its Challenger Bage"/>
+        default:
+            return null
+    }
+}
+const getType= (type: string)=>{
+    switch (type){
+        case 'RANKED_FLEX_SR':
+            return 'FLEX'
+        case 'RANKED_SOLO_5x5':
+            return 'SOLO Q'
+        default: return'Brak rankedów!'
+    }
+}
+
+interface Props{
+    stats: LeaguesEntity
+}
+const EnemyStats = ({stats}: Props) => {
+
+    return (
+        <div>
+            <div>{getType(stats.queueType)}</div>
+            <div>
+                {getImage(stats.tier)}
+                <div>
+                    <p>{stats.tier} {stats.rank}</p>
+                    <p>{stats.leaguePoints} League Points</p>
+                    <p>Wins / Losses</p>
+                    <p>{stats.wins} / {stats.losses}</p>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default EnemyStats;
