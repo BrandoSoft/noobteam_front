155f3e3197c153c680369510952bda4fb61bfcbe
BrandoSoft <csbrandox@gmail.com>
2022-10-03 22:54:37 +0200

    modal is able to take player historry

--- src/components/EnemyCard/SummonerHistoryGames.tsx
+++ src/components/EnemyCard/SummonerHistoryGames.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {LeaguesEntity} from 'types'
 import './EnemyCard.scss'
+import {getGameScore, getHistoryGamesIds} from "../../apiCalls/matches";
+import {Scores} from "./EnemyHistory";
+import {useSelector} from "react-redux";
+import {RootState} from "../../redux/store";
+import {getSummonerData} from "../../apiCalls/characters";
+import {v4 as uuidv4} from "uuid";
 
 interface Props {
     info: LeaguesEntity,
     hideModal: ()=>void
 }
 
-const SummonerHistoryGames = ({info}: Props) => {
+const SummonerHistoryGames = ({info, hideModal}: Props) => {
+
+    const {userToken} = useSelector((store: RootState) => store.user);
+    const version = process.env.REACT_APP_DDRAGON;
+
+    const [list, setList]= useState<Scores[]>([]);
+
+    useEffect(() => {
+
+            (async () => {
+                try {
+                    const summonerData = await getSummonerData(info.summonerName, userToken);
+
+                    const gameHistoryIds = await getHistoryGamesIds(summonerData.puuid, userToken, 10)
+                    const gamesHistoryList = await getGameScore(gameHistoryIds, userToken);
+                    await setList(gamesHistoryList)
+                } catch (e) {
+                    console.log(e)
+                }
+            })()
+
+        }, []
+    )
+    console.log(`lista w karcie postaci`,list)
+
     return (
         <div className="summonerModal">
-            {info.summonerId}
+                        {info.summonerName}
+            {list && list.map(item =>( <div key={uuidv4()} className="history__container">
+                <img className="history__small"
+                     src={`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${item.name}.png`}
+                     alt=""/>
+                <div style={{backgroundColor:`${item.win? "red":"green"}`, width:"100%"}}
+                >Z: {item.kills} / Z: {item.deaths} / A: {item.assists}  <p>{item.role} / {item.lane}</p></div>
+            </div>))
+            }
+            <button onClick={hideModal}> X</button>
         </div>
     );
 };
