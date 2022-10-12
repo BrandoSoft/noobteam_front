027c9ca8cd649bc118ae2af7c7d17dab915b7a38
BrandoSoft <csbrandox@gmail.com>
2022-10-12 19:56:31 +0200

    fix: fixed bug with ddragon api

--- src/apiCalls/ddragon.ts
+++ src/apiCalls/ddragon.ts
@@ -2,15 +2,15 @@
 const version = process.env.REACT_APP_DDRAGON;
 
 export const getChampData = async (): Promise<any>=>{
-    const resChamp = await fetch(`http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`)
+    const resChamp = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`)
 
     return await resChamp.json()
 }
 
 export const getPlayerAvatar = (name: string): string =>{
-    return `http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${name}.png`
+    return `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${name}.png`
 }
 
 export const getSummonerAvatar = (profileIconId:string): string =>{
-    return `http://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${profileIconId}.png`
+    return `https://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${profileIconId}.png`
 }
